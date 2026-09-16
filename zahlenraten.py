f0d76abf88400c8f82561ad7e52cbad1515045fd
Salome Spannring <salome.spannring@outlook.com>
2026-09-16 14:21:23 +0200

    Add Zahlenraten program

---
+++ zahlenraten.py
@@ -0,0 +1,21 @@
+import random
+geheimzahl = random.randint(1, 50)
+
+print("Salut!")
+print("Rate, an welche Zahl ich gerade denke.")
+print("Zwischen 1 und 50.")
+
+guess = int(input())
+count = 1
+
+while guess != geheimzahl:
+    if guess < geheimzahl:
+        print("Zu tief!")
+    elif guess > geheimzahl:
+        print("Zu hoch!")
+
+    guess = int(input("Nächster Versuch: "))
+    count += 1
+
+print("Jackpot!")
+print(f"Du hast {count} Versuche gebraucht.")
