5842dfd41b83d791fc71be55d29b1bcefead4928
Salome Spannring <salome.spannring@outlook.com>
2026-09-16 14:07:27 +0200

    Add Alter program

---
+++ alter.py
@@ -0,0 +1,16 @@
+print("Hallo! Wie ist dein Name?")
+name = input()
+
+print("Und wie alt bist du?")
+age = input()
+
+print(f"Hallo {name}!")
+
+age = int(age)
+if age < 18:
+    print("Du bist minderjährig.")
+elif age >= 18 and age <= 64:
+    print("Du bist im arbeitsfähigen Alter.")
+else:
+    print("Du bist im Rentenalter.")
+
